41fdc52d9be18782d5f3d8af5fa42d1df4b8a4f0
Chris Nunez <cenunez47@gmail.com>
2018-04-06 00:55:48 +0000

    Done.

--- index.js
+++ index.js
@@ -13,12 +13,11 @@ function theBeatlesPlay(musicians, instruments) {
 //
 
 function johnLennonFacts(facts) {
-  var newArr = []
-  var i = 0
+  let i = 0
   while (i < facts.length) {
-    newArr.push(facts[i] + '!!!'); i++;
+    console.log(facts[i] + '!!!');
+    i++;
   }
-  return newArr;
 }
 
 //
