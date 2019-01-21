ed32ebf0ff54a41b8037a6cbb6d84747127da8dc
Joe Corwin <joe.corwin@gmail.com>
2019-01-21 20:16:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+var beatlesPlay = []
+
+function theBeatlesPlay(musicians, instruments) {
+  for (let n = 0; n < musicians.length; n++) {
+    beatlesPlay.push(`${musicians[n]} plays ${instruments[n]}`)
+  }
+  return beatlesPlay
+}
+
+function johnLennonFacts(facts) {
+  let exclamArray = [];
+  let n = 0;
+  while (n < facts.length) {
+    exclamArray.push(`${facts[n]}!!!`);
+    n++
+  }
+  return exclamArray
+}
+
+function iLoveTheBeatles(n) {
+  let iLoveArray = [];
+  do{
+    iLoveArray.push('I love the Beatles!');
+    n++
+  } while (n < 15);
+  return iLoveArray
+}
